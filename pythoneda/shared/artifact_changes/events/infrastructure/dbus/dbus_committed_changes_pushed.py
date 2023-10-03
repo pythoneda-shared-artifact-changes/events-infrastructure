f4c204d70e1eeb09ddb5b1d6add859d795cb6512
rydnr <github@acm-sl.org>
2023-10-03 17:24:50 +0200

    Fixed bugs

---
+++ pythoneda/shared/artifact_changes/events/infrastructure/dbus/dbus_committed_changes_pushed.py
@@ -0,0 +1,100 @@
+"""
+pythoneda/shared/artifact_changes/events/infrastructure/dbus/dbus_committed_changes_pushed.py
+
+This file defines the DbusCommittedChangesPushed class.
+
+Copyright (C) 2023-today rydnr's pythoneda-shared-artifact-changes/event-infrastructure
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+from dbus_next import Message
+from dbus_next.service import ServiceInterface, signal
+import json
+from pythoneda import BaseObject
+from pythoneda.shared.artifact_changes.events import CommittedChangesPushed
+from pythoneda.shared.artifact_changes.events.infrastructure.dbus import DBUS_PATH
+from typing import List
+
+class DbusCommittedChangesPushed(BaseObject, ServiceInterface):
+    """
+    D-Bus interface for CommittedChangesPushed
+
+    Class name: DbusCommittedChangesPushed
+
+    Responsibilities:
+        - Define the d-bus interface for the CommittedChangesPushed event.
+
+    Collaborators:
+        - None
+    """
+
+    def __init__(self):
+        """
+        Creates a new DbusCommittedChangesPushed.
+        """
+        super().__init__(
+            "Pythoneda_Shared_artifact_changes_Events_CommittedChangesPushed"
+        )
+
+    @signal()
+    def CommittedChangesPushed(self, change: "s"):
+        """
+        Defines the CommittedChangesPushed d-bus signal.
+        :param change: The change.
+        :type change: str
+        """
+        pass
+
+    @classmethod
+    def path(cls) -> str:
+        """
+        Retrieves the d-bus path.
+        :return: Such value.
+        :rtype: str
+        """
+        return DBUS_PATH
+
+    @classmethod
+    def transform(self, event: CommittedChangesPushed) -> List[str]:
+        """
+        Transforms given event to signal parameters.
+        :param event: The event to transform.
+        :type event: pythoneda.shared.artifact_changes.events.CommittedChangesPushed
+        :return: The event information.
+        :rtype: List[str]
+        """
+        return [ str(event.change), event.commit, event.id, json.dumps(event.previous_event_ids) ]
+
+    @classmethod
+    def sign(cls, event: CommittedChangesPushed) -> str:
+        """
+        Retrieves the signature for the parameters of given event.
+        :param event: The domain event.
+        :type event: pythoneda.shared.artifact_changes.events.CommittedChangesPushed
+        :return: The signature.
+        :rtype: str
+        """
+        return 'ssss'
+
+    @classmethod
+    def parse(cls, message: Message) -> CommittedChangesPushed:
+        """
+        Parses given d-bus message containing a CommittedChangesPushed event.
+        :param message: The message.
+        :type message: dbus_next.Message
+        :return: The CommittedChangesPushed event.
+        :rtype: pythoneda.shared.artifact_changes.events.CommittedChangesPushed
+        """
+        change_json, commit, event_id, prev_event_ids = message.body
+        return CommittedChangesPushed(Change.from_json(change_json), commit, None, event_id, json.loads(prev_event_ids))
